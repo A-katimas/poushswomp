59d26995b4c53b57f5623321e334366f1f76c4b2
Axel Heno <aheno@k2r3p8.42mulhouse.fr>
2026-02-06 09:44:54 +0100

    chunk sort modif

--- orders/chunk_sort_utils2.c
+++ orders/chunk_sort_utils2.c
@@ -6,42 +6,32 @@
 /*   By: aheno <aheno@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/23 16:18:11 by aheno             #+#    #+#             */
-/*   Updated: 2026/02/05 16:03:25 by aheno            ###   ########.fr       */
+/*   Updated: 2026/02/06 09:34:53 by aheno            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "order.h"
 
-int	ft_sqrt(int nb)
+static int	ft_sqrt(int nb)
 {
 	int	i;
-	int	res;
 
-	i = 0;
-	res = 0;
-	if (nb < 0)
-	{
-		return (1);
-	}
-	while (i <= 43460 && res < nb)
-	{
-		res = i * i;
-		if (res == nb)
-		{
-			return (i);
-		}
+	if (nb <= 0)
+		return (0);
+	i = 1;
+	while (i <= 46340 && i * i <= nb)
 		i++;
-	}
-	return (0);
+	return (i - 1);
 }
 
 int	get_chunk_count(int size)
 {
-	if (size <= 10)
-		return (1);
-	if (size <= 100)
-		return (5);
-	if (size <= 500)
-		return (11);
-	return (15);
+	int	count;
+
+	if (size <= 0)
+		return (0);
+	count = ft_sqrt(size);
+	if (count < 1)
+		count = 1;
+	return (count);
 }
